194c12e4f411b69080832cd7a05fac5c49070420
PiotrCoder-creator <petia3711@gmail.com>
2023-10-07 11:04:24 +0300

    changed insertWords, doubleEachLetter, replaceLatinWithRussian and added vowelsCount method.

--- src/main/scala/Lab3.scala
+++ src/main/scala/Lab3.scala
@@ -1,5 +1,11 @@
+import scala.util.matching.Regex
+
 object TextProcessingApp {
   def main(args: Array[String]): Unit = {
+    val text0 = "Hello, Scala 3 !"
+    val countedText = vowelsCount(text0)
+    println("0. Количество гласных в тексте: " + countedText)
+
     val text1 = "Hello to everybody"
     val replacedText = replaceLatinWithRussian(text1)
     println("1. Замененный текст: " + replacedText)
@@ -33,15 +39,24 @@ object TextProcessingApp {
     println("8. Текст с вставленными словами: " + insertedText.mkString(" "))
   }
 
-  def replaceLatinWithRussian(text: String): String = {
-    val replacementMap = Map(
-      'H' -> 'Н', 'e' -> 'е', 'l' -> 'l', 'o' -> 'о',
-      't' -> 'т', 'o' -> 'о', ' ' -> ' ', 'e' -> 'е',
-      'v' -> 'v', 'r' -> 'г', 'y' -> 'у', 'b' -> 'ь',
-      'd' -> 'd', 'y' -> 'у'
-    )
-    text.map(char => replacementMap.getOrElse(char, char))
-  }
+//  def replaceLatinWithRussian(text: String): String = { // no
+//    val replacementMap = Map(
+//      'H' -> 'Н', 'e' -> 'е', 'l' -> 'l', 'o' -> 'о',
+//      't' -> 'т', 'o' -> 'о', ' ' -> ' ', 'e' -> 'е',
+//      'v' -> 'v', 'r' -> 'г', 'y' -> 'у', 'b' -> 'ь',
+//      'd' -> 'd', 'y' -> 'у'
+//    )
+//    text.map(char => replacementMap.getOrElse(char, char))
+//  }
+    def replaceLatinWithRussian(text: String): String = {
+//      val text: String = "Hello to everybody"
+//      println("Source: " + text)
+      val regex: Regex = """[a-zA-Z]""".r
+      /*val newText: String = */regex.replaceAllIn(text, { c => (c.toString + 32).toString })
+//      return(newText)
+//      println("Result: " + newText)
+}
+
 
   def findWordsStartingWithExt(text: String): List[String] = {
     val extWordsRegex = "\\bext\\w*".r
@@ -60,21 +75,49 @@ object TextProcessingApp {
     text.replaceAll("[aeiouAEIOU]", "")
   }
 
+//  def doubleEachLetter(text: String): String = { // no
+//    text.flatMap(char => s"$char$char")
+//  }
   def doubleEachLetter(text: String): String = {
-    text.flatMap(char => s"$char$char")
+//    var text: String = "Hello to everybody"
+//    println("Source: " + text)
+    val regex: Regex = """\w""".r
+    /*val newText: String =*/ regex.replaceAllIn(text, { m => m.group(0) + m.group(0) })
+//    return newText
+//    println("Result: " + newText)
   }
+
 
   def removeLetterY(text: String): String = {
     text.replaceAll("y", "")
   }
 
+//  def insertWords(text: String, wordsToInsert: String, position: Int): String = { // no
+//    val words = text.split(' ')
+//    if (position >= 0 && position <= words.length) {
+//      val (before, after) = words.splitAt(position)
+//      (before :+ wordsToInsert).mkString(" ") + " " + after.mkString(" ")
+//    } else {
+//      text // Return original text if position is invalid
+//    }
+//  }
+
   def insertWords(text: String, wordsToInsert: String, position: Int): String = {
-    val words = text.split(' ')
-    if (position >= 0 && position <= words.length) {
-      val (before, after) = words.splitAt(position)
-      (before :+ wordsToInsert).mkString(" ") + " " + after.mkString(" ")
-    } else {
-      text // Return original text if position is invalid
-    }
+    val text: String = "Hello to everybody"
+    println("Source: " + text)
+    val regex: Regex = """\bto\b""".r
+    /*var newText: String =*/ regex.replaceAllIn(text, "with heartness to")
+//    println("Result: " + newText)
   }
+
+  def vowelsCount(text: String): Int = {
+//    print("Input text: ")
+//    val text: String = scala.io.StdIn.readLine()
+    val regex: Regex = """[aeiouyAEIOUY]""".r
+    /*val matches =*/ regex.findAllMatchIn(text).length
+//    println("Vowels count: " + matches.size)
+  }
+
+
 }
+
